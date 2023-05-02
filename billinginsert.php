39d11bcf3e2ddf03c175cba35fd8e4c65606dc03
ShairaHonrejas <honrejasshaira@gmail.com>
2023-05-02 21:21:30 +0800

    update the billinginsert.php

--- billinginsert.php
+++ billinginsert.php
@@ -11,14 +11,15 @@ $o_quant = $_POST['quantity'];
       while ($row = $result ->fetch_assoc())
       {
         if($row['p_id'] == $p_id){
-          echo "already have the product";
           $row ['o_quantity'];
           $updatequant = $o_quant + $row['o_quantity'];
           $sql2 ="UPDATE orders SET o_quantity = '$updatequant'";
           $result2 = mysqli_query($connection, $sql2);
           if ($result2 == "TRUE")
           {
-            echo "update Successfully";
+            echo  
+              header("location:home.php")
+            ;
           }
           else
           {
@@ -32,13 +33,14 @@ $o_quant = $_POST['quantity'];
       }
     }
     else{
-      echo "you still dont have the product";
       $sql1 = "INSERT into orders (o_name , p_id , o_price, o_quantity  ) VALUES ( '$name' , '$p_id' , '$price' , '$o_quant' )";
       $result1 = mysqli_query($connection , $sql1);
       if ($result1 == "TRUE")
       {
-        echo "insert Success";
-      }
+        echo  
+        header("location:home.php")
+      ;
+}
       else
       {
         echo "insert error";
@@ -46,16 +48,3 @@ $o_quant = $_POST['quantity'];
     }
           
         
-        if($result == "TRUE"){
-           echo "<script> 
-                      alert('Order Entered');
-                      window.location.href='home.php'
-                </script>"; 
-        }  
-        else{  
-            echo "<script> 
-                    alert('Something went wrong!');
-                    window.location.href='home.html'
-                </script>";
-        }      
-?>
