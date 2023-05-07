eaf270ead36ece7f3d7a933b21b3ddd2a15c524e
ShairaHonrejas <honrejasshaira@gmail.com>
2023-05-07 22:07:21 +0800

    edit the register.php

--- register.php
+++ register.php
@@ -3,7 +3,7 @@ include "connection.php";
 $name = $_POST['name'];
 $user = $_POST['username'];
 $pass = $_POST['password'];
-      $sql = "INSERT into employee (e_name , e_user , e_pass  ) VALUES ( '$name' , '$user' , '$pass'  )";
+      $sql = "INSERT into employees (e_name , e_user , e_pass  ) VALUES ( '$name' , '$user' , '$pass'  )";
       $result = mysqli_query($connection , $sql);
       if ($result == "TRUE")
       {
